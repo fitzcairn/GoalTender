ac719aec86bd50b6438ca1b7c05f7e6f9e072de8
Steve Martin <steve@stevezero.com>
2018-05-31 18:26:03 -0700

    Silence the isMounted warnings.

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 
+// Courtesy of https://github.com/react-navigation/react-navigation/issues/3956.
+import { YellowBox } from 'react-native';
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
+
 AppRegistry.registerComponent('GoalTender', () => App);
