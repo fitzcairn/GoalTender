cd23d6ca574d47e4be809619359308ec45059f1d
Steve Martin <steve@stevezero.com>
2018-06-12 09:57:59 -0700

    Refactored the goal screen now that I understand how to work with components better.

--- src/app/screens/StatsScreen.js
+++ src/app/screens/StatsScreen.js
@@ -25,6 +25,7 @@ import {
 
 import Swiper from 'react-native-swiper';
 
+import GoalHistoryView from '../components/GoalHistoryView.js';
 import IconButton from '../components/IconButton.js';
 import LoadingSpinner from '../components/LoadingSpinner.js';
 
@@ -33,7 +34,7 @@ import UserService from '../services/UserService.js';
 
 import { User } from '../storage/data/User.js';
 import { Goal, GoalList } from '../storage/data/Goal.js';
-import { StateValues } from '../storage/data/State.js';
+import { State as StateForGoal, StateValues } from '../storage/data/State.js';
 
 import { dateDisplay, getWeekdays, getDaysBetweenDisplay } from '../Dates.js';
 
@@ -52,9 +53,7 @@ type State = {
   dataLoaded: boolean,
   showCompleted: boolean,
   user: User | null,
-  goalStates: Map<string, Map<string, State>>,
   goals: GoalList | null,
-  lastIndex: number,
 };
 
 // Stats Screen
@@ -108,9 +107,6 @@ export default class StatsScreen extends Component<Props, State> {
       user: user,
       goals: goals,
     });
-
-    // Load state for first screen in swiper.
-    this._loadStatesFor(0);
   }
 
   _refreshData() {
@@ -124,9 +120,7 @@ export default class StatsScreen extends Component<Props, State> {
         if (this.state.showCompleted) {
           GoalService.getCompletedGoals(
             user.getId(),
-            (goals: GoalList) => {
-              this._refreshDataState(user, goals);
-            }
+            (goals: GoalList) => this._refreshDataState(user, goals)
           ).catch((error) => {
             log("StatsScreen -> _refreshData -> getCompletedGoals: " + error);
           });
@@ -134,9 +128,7 @@ export default class StatsScreen extends Component<Props, State> {
         } else {
           GoalService.getIncompleteGoals(
             user.getId(),
-            (goals: GoalList) => {
-              this._refreshDataState(user, goals);
-            }
+            (goals: GoalList) => this._refreshDataState(user, goals)
           ).catch((error) => {
             log("StatsScreen -> _refreshData -> getGoals: " + error);
           });
@@ -147,126 +139,10 @@ export default class StatsScreen extends Component<Props, State> {
     });
   }
 
-  _loadStatesFor(index: number) {
-    if (!this.state.dataLoaded || this.state.goals == null) return;
-    const goals:Array<Goal> = this.state.goals.getGoals();
-
-    if (goals.length <= 0) return;
-    const goal:Goal = goals[index];
-
-    if (this.state.user == null) return;
-    const userId:string = this.state.user.getId();
-
-    if (this.state.goalStates == null) return;
-    const goalStates:Map<string, Map<string, State>> = this.state.goalStates;
-
-    // Load the state for the first goal.
-    GoalService.getStatesForGoal(
-      userId,
-      goal.getId(),
-      (stateMap: Map<string, State>) => {
-        goalStates.set(goal.getId(), stateMap);
-
-        // Success!  Set state and trigger refresh.
-        this.setState({
-          dataLoaded: true,
-          lastIndex: index,
-        });
-      }
-    ).catch((error) => {
-      log("StatsScreen -> _loadStatesFor: " + error);
-    });
-  }
-
-  _renderWeekDays(): Node {
-    const weekdays:Array<string> = getWeekdays();
-    const rendered:Array<Object> = weekdays.map((day: string) => {
-      return (
-          <Text key={day} style={styles.calendarWeekdaysText}>
-            { day.toUpperCase() }
-          </Text>
-      );
-    });
-    return rendered;
-  }
-
-  _renderDays(days: Array<string>, dateMap: Map<string, State>): Node {
-    return days.map((day: string, index: number) => {
-      return (
-        <View key={index} style={
-          (dateMap.has(day[0]) ?
-            (dateMap.get(day[0]).getState() == StateValues.NO ?
-              styles.dayNo : styles.dayYes) :
-            styles.dayNone
-          )}>
-          <Text style={
-            (dateMap.has(day[0]) ? styles.dayTextColor : styles.dayText)
-          }>
-            { day[1] }
-          </Text>
-        </View>
-      );
-    });
-  }
-
-  _renderWeeks(goal: Goal): Node {
-    if (!this.state.goalStates.has(goal.getId()))
-      return (
-        <LoadingSpinner modal={false} />
-      );
-
-    // Get days between the first and last date we have data.
-    const dateMap:Map<string, State> = this.state.goalStates.get(goal.getId());
-    const dateList = Array.from(dateMap.keys());
-    const days:Array<Array<string>> = getDaysBetweenDisplay(dateList[0],
-      dateList[dateList.length - 1]);
-
-    // Create weeks.
-    const weeks:Array<Array<Array<string>>> = [];
-    for (let day = 0; day < days.length; day += 7)
-      weeks.push(days.slice(day, day + 7));
-
-    // Render grid.
-    return weeks.map((week: Array<Array<string>>, index: number) => {
-      return (
-          <View key={index} style={styles.week}>
-            { this._renderDays(weeks[index], dateMap) }
-          </View>
-      );
-    });
-  }
-
-  _renderGoalText(goal: Goal) {
-    if (goal.getComplete())
-      return (
-        <View style={styles.headerItem}>
-          <Text style={styles.goalCompleted}>
-            { Localized("Stats.completed") }
-          </Text>
-          <Text style={styles.goalText}>
-            { goal.getText() }
-          </Text>
-          <Text style={styles.dateText}>
-          { /* Note: Can't seem to get I18n.t working with params. */ }
-          { Localized("Stats.created") + dateDisplay(goal.getCreateDate()) }
-          </Text>
-        </View>
-      );
-    return (
-      <View style={styles.headerItem}>
-        <Text style={styles.goalText}>
-          { goal.getText() }
-        </Text>
-        <Text style={styles.dateText}>
-          { /* Note: Can't seem to get I18n.t working with params. */ }
-          { Localized("Stats.created") + dateDisplay(goal.getCreateDate()) }
-        </Text>
-      </View>
-    );
-}
-
   _renderGoals(): Node {
-    const goals:Array<Goal> = this.state.goals.getGoals();
+    const userId:?string = this.state.user.getId();
+    const goalList:?GoalList = this.state.goals;
+    const goals:Array<Goal> = (goalList == null? [] : goalList.getGoals());
 
     if (goals.length == 0)
       return (
@@ -281,17 +157,15 @@ export default class StatsScreen extends Component<Props, State> {
 
     return goals.map((goal: Goal, index: number) => {
       return (
-        <View key={index}>
-          <View style={styles.header}>
-              { this._renderGoalText(goal) }
-          </View>
-          <View style={styles.calendarWeekdays}>
-              { this._renderWeekDays() }
-          </View>
-          <ScrollView>
-            { this._renderWeeks(goal) }
-          </ScrollView>
-        </View>
+        <GoalHistoryView
+          key={index}
+          index={index}
+          userId={userId}
+          goalId={goal.getId()}
+          goalText={goal.getText()}
+          goalDate={goal.getCreateDate()}
+          goalComplete={goal.getComplete()}
+        />
       );
     });
   }
@@ -306,7 +180,6 @@ export default class StatsScreen extends Component<Props, State> {
         showsButtons={false}
         loadMinimal={true}
         showsPagination={true}
-        onIndexChanged={(index) => { this._loadStatesFor(index) }}
       >
         { this._renderGoals() }
       </Swiper>
@@ -317,79 +190,5 @@ export default class StatsScreen extends Component<Props, State> {
 // Local styles.
 const styles = StyleSheet.create({
   swiper: {
-  },
-  week: {
-    flexDirection: 'row',
-  },
-  dayNone: {
-    flex: 1,
-    backgroundColor: '#FFF',
-    paddingTop: 12,
-    paddingBottom: 12,
-    margin: 1
-  },
-  dayNo: {
-    flex: 1,
-    backgroundColor: '#cc0000',
-    paddingTop: 12,
-    paddingBottom: 12,
-    margin: 1,
-  },
-  dayYes: {
-    flex: 1,
-    backgroundColor: '#006600',
-    paddingTop: 12,
-    paddingBottom: 12,
-    margin: 1,
-  },
-  dayText: {
-    flex: 1,
-    textAlign: 'center',
-    color: '#595959',
-    fontSize: 12
-  },
-  dayTextColor: {
-    flex: 1,
-    textAlign: 'center',
-    color: '#f2f2f2',
-    fontSize: 12
-  },
-  calendarWeekdays: {
-    flexDirection: 'row',
-  },
-  calendarWeekdaysText: {
-    flex: 1,
-    color: '#808080',
-    textAlign: 'center',
-    marginTop: 4,
-    marginBottom: 4,
-  },
-  header: {
-    flexDirection: 'row',
-    padding: 10
-  },
-  headerItem: {
-    flex: 1,
-    alignItems: 'center',
-  },
-  goalText: {
-    textAlign: 'center',
-    fontSize: Platform.OS === 'ios' ? 16 : 18,
-    color: 'rgba(0, 0, 0, .9)',
-    marginHorizontal: 16,
-  },
-  goalCompleted: {
-    textAlign: 'center',
-    fontSize: Platform.OS === 'ios' ? 10 : 12,
-    color: '#006600'
-  },
-  dateText: {
-    textAlign: 'center',
-    fontSize: Platform.OS === 'ios' ? 10 : 12,
-    color: 'grey'
-  },
-  dayScrollView: {
-    alignSelf: 'center',
-    marginTop: 100,
   },
 });
